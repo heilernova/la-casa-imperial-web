d3d60c084a1d9cfb1a8ff28430f95a0f82ee4d9f
Heiler Nova <heilernova@outlook.com>
2024-09-22 21:33:52 -0500

    Add: barra de navegación

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderContentComponent } from './header-content/header-content.component';
+import { NavContentComponent } from './nav-content/nav-content.component';
+import { ListProductsComponent } from "../components/list-products/list-products.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
-    HeaderContentComponent
-  ],
+    HeaderContentComponent,
+    NavContentComponent,
+    ListProductsComponent
+],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
--- src/app/layout/nav-content/nav-content.component.ts
+++ src/app/layout/nav-content/nav-content.component.ts
@@ -8,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './nav-content.component.scss'
 })
 export class NavContentComponent {
-
+  list = [
+    {
+      name: "Neveras y nevecones",
+      link: "electrodomesticos/refrigeracion/neveras-y-nevecones"
+    },
+    {
+      name: "Congeladores",
+      link: "electrodomesticos/refrigeracion/congeladores"
+    },
+    {
+      name: "Lavadoras",
+      link: "electrodomesticos/lavado/lavadoras"
+    },
+    {
+      name: "Televisores",
+      link: "electrodomesticos/lavado/lavadoras"
+    },
+    {
+      name: "Cocina",
+      link: "electrodomesticos/lavado/lavadoras"
+    },
+    {
+      name: "Climatización",
+      link: "electrodomesticos/lavado/lavadoras"
+    },
+    {
+      name: "Cuidad del hogar",
+      link: "electrodomesticos/lavado/lavadoras"
+    },
+  ];
 }
